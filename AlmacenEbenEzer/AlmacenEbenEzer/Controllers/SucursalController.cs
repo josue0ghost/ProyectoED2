290cabc4488c76441de40b5ce9b226e55688a4d5
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-04-28 13:25:59 -0600

    C,U for "Sucursal"

---
+++ AlmacenEbenEzer/AlmacenEbenEzer/Controllers/SucursalController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlmacenEbenEzer.Controllers
+{
+    public class SucursalController : Controller
+    {
+        // GET: Sucursal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Sucursal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sucursal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sucursal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sucursal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sucursal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sucursal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sucursal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
